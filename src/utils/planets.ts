5f4d18da332dd3f485c33bed2836c0082ee3b50f
César Rincón <90701896+CesarRincon@users.noreply.github.com>
2025-02-02 15:34:06 -0500

    AP-ADD: basic data of planets added

---
+++ src/utils/planets.ts
@@ -0,0 +1,56 @@
+export const planetsData = [
+    {
+        id: 1,
+        name: 'Mercury',
+        image: 'https://static.wikia.nocookie.net/planetpedia/images/7/72/MercuryCGIpromo.GIF/revision/latest?cb=20160715132914',
+        distanceFromEarth: 91.7 
+    },
+    {
+        id: 2,
+        name: 'Venus',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Venus_globe_-_transparent_background.png/800px-Venus_globe_-_transparent_background.png',
+        distanceFromEarth: 41.4 
+    },
+    {
+        id: 3,
+        name: 'Earth',
+        image: 'https://static.wikia.nocookie.net/id4/images/1/1e/Earth.png/revision/latest?cb=20211124210248',
+        distanceFromEarth: 0 
+    },
+    {
+        id: 4,
+        name: 'Mars',
+        image: 'https://static.wikia.nocookie.net/id4/images/f/f3/Mars_Reader.png/revision/latest/scale-to-width-down/1000?cb=20211227201123',
+        distanceFromEarth: 78.3 
+    },
+    {
+        id: 5,
+        name: 'Jupiter',
+        image: 'https://www.solarsystemscope.com/spacepedia/images/handbook/renders/jupiter.png',
+        distanceFromEarth: 628.7 
+    },
+    {
+        id: 6,
+        name: 'Saturn',
+        image: 'https://png.pngtree.com/png-vector/20240805/ourmid/pngtree-abstract-image-with-saturn-planet-wit-satellites-3d-render-illustration-png-image_13387351.png',
+        distanceFromEarth: 1_275 
+    },
+    {
+        id: 7,
+        name: 'Uranus',
+        image: 'https://png.pngtree.com/png-clipart/20241001/original/pngtree-a-beautiful-uranus-planet-png-image_16139577.png',
+        distanceFromEarth: 2_723 
+    },
+    {
+        id: 8,
+        name: 'Neptune',
+        image: 'https://www.solarsystemscope.com/spacepedia/images/handbook/renders/neptune.png',
+        distanceFromEarth: 4_351 
+    },
+    {
+        id: 9,
+        name: 'Pluto',
+        image: 'https://www.solarsystemscope.com/spacepedia/images/handbook/renders/pluto.png',
+        distanceFromEarth: 5_000 
+    }
+];
